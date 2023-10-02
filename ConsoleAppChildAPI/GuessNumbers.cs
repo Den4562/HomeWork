77d62339acf4e561d9aeb37e9b45711787f9c70b
Den4562 <119700990+Den4562@users.noreply.github.com>
2023-10-02 21:49:25 +0300

    Create apllication with use WinAPI

---
+++ ConsoleAppChildAPI/GuessNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppChildAPI
+{
+    public class GuessNumbers
+    {
+        public void PlayGame()
+        {
+            Random random = new Random();
+            int min = 0;
+            int max = 100;
+            int num = random.Next(min, max + 1);
+            int attempts = 0;
+            int userGuess;
+
+            Console.WriteLine($"Попробуйте угадать число от {min} до {max}.");
+
+            do
+            {
+                Console.Write("Введите число: ");
+                if (int.TryParse(Console.ReadLine(), out userGuess))
+                {
+                    attempts++;
+                    if (userGuess < num)
+                    {
+                        Console.WriteLine("Загаданное число больше.");
+                    }
+                    else if (userGuess > num)
+                    {
+                        Console.WriteLine("Загаданное число меньше.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ви угадали число {num} за {attempts} попыток.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Пожалуйста введите правельное число.");
+                }
+            } while (userGuess != num);
+
+        }
+    }
+}
---
+++ ConsoleAppChildAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleAppChildAPI;
+using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    
+    
+    
+    
+    // Імпортуємо функцію MessageBox з Windows API
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    static void Main()
+    {
+      
+        MessageBox(IntPtr.Zero, "Hello, World!", "MessageBox Example", 0);
+
+
+
+        Console.WriteLine("Игра: 'Угадайте число'");
+        do
+        {
+            GuessNumbers game = new GuessNumbers();
+            game.PlayGame();
+            Console.WriteLine("Хотите сыграть еще раз ? (Да или нет)");
+        } while (Console.ReadLine()?.ToLower() == "да");
+    }
+}
